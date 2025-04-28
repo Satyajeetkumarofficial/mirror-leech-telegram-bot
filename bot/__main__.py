7509211c9cb412531e3b19071cd3b1ac430fa58e
Satyajeetkumarofficial <152394869+Satyajeetkumarofficial@users.noreply.github.com>
2025-04-28 11:45:55 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,6 +1,19 @@
 from . import LOGGER, bot_loop
 from .core.mltb_client import TgClient
 from .core.config_manager import Config
+from threading import Thread
+from flask import Flask
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is alive", 200
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+Thread(target=run).start()
 
 Config.load()
 
